122709920db2ca1f5597e0b6a8f3dfe98e4e6ca6
Murat Artan <muratartan@hotmail.com>
2022-09-01 18:55:33 +0300

    set file pattern

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use std::io;
 
-async fn index() -> impl Responder {
+async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello World")
 }
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     println!("listening on port 8080");
-    HttpServer::new(|| App::new().route("/hello", web::get().to(index)))
+    HttpServer::new(|| App::new().route("/hello", web::get().to(hello)))
         .bind("127.0.0.1:8080")?
         .run()
         .await
