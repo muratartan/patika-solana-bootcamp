f85f2bd83452e3f2e7500a026930da1d79c147be
Murat Artan <muratartan@hotmail.com>
2022-09-01 16:35:46 +0300

    set hello handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
+}
+
 fn main() {
     println!("Hello, world!");
 }
